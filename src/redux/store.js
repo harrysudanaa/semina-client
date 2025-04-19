ff06feafbf11b1838464f135def1174f62497053
harrysudanaa <komangharry02@gmail.com>
2025-04-19 11:57:29 +0800

    change: finish crud talents and crud payments

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,6 +9,8 @@ import { thunk } from "redux-thunk";
 import authReducer from "./auth/reducer";
 import categoriesReducer from "./categories/reducer";
 import notifReducer from "./notif/reducer";
+import talentsReducer from "./talents/reducer";
+import paymentsReducer from "./payments/reducer";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -16,6 +18,8 @@ const rootReducers = combineReducers({
   auth: authReducer,
   categories: categoriesReducer,
   notif: notifReducer,
+  talents: talentsReducer,
+  payments: paymentsReducer,
 });
 
 const store = createStore(
